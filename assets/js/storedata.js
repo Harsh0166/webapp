b6d8508d3e5b51167d721c15f64e74cf66e93014
Harsh0166 <2005harshkumar@gmail.com>
2024-10-22 23:01:46 +0530

    update

--- assets/js/storedata.js
+++ assets/js/storedata.js
@@ -6,10 +6,10 @@ var sub_btn = document.getElementById("sub_btn");
 var warnning = document.getElementById("warn");
 
 signup_form.onsubmit=function(){
-    var names = document.getElementById("name").value;
-    var pass = document.getElementById("passs").value;
-    var phone = document.getElementById("phone").value; 
-    var email = document.getElementById("email").value;
+    var names = btoa(document.getElementById("name").value);
+    var pass = btoa(document.getElementById("passs").value);
+    var phone = btoa(document.getElementById("phone").value); 
+    var email = btoa(document.getElementById("email").value);
     var user_object_data = {name:names,email:email,password:pass,phone:phone};
     var user_text_data = JSON.stringify(user_object_data);
 
@@ -34,12 +34,12 @@ signup_form.onsubmit=function(){
 
 }
 
-// email process 
+// email validation 
 
 
 var email_input = document.getElementById("email");
 email_input.onchange = function(){
-    var email = document.getElementById("email").value;
+    var email = btoa(document.getElementById("email").value);
     if(localStorage.getItem(email)!= null){
         warnning.style.display="block";
         email_input.style.borderBottomColor="red";
@@ -62,8 +62,8 @@ var log_warn = document.getElementById("log_warn");
 var log_warns = document.getElementById("log_warns");
 
 login_form.onsubmit= function(){
-var log_email = document.getElementById("log_email").value;
-var log_pass = document.getElementById("log_pass").value;
+var log_email = btoa(document.getElementById("log_email").value);
+var log_pass = btoa(document.getElementById("log_pass").value);
 
     if(localStorage.getItem(log_email) == null ){
         log_warn.innerHTML="<i class='fa-solid fa-triangle-exclamation'></i> email not found";
@@ -83,5 +83,5 @@ var log_pass = document.getElementById("log_pass").value;
             }
         }
     }
-    
+    return false;
 }
