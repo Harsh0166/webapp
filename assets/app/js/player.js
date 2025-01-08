ade71fa7138a3207ba37e5162216bf31c2d68eb1
Harsh0166 <2005harshkumar@gmail.com>
2025-01-08 09:52:09 +0530

    up

--- assets/app/js/player.js
+++ assets/app/js/player.js
@@ -19,10 +19,13 @@ player.ontimeupdate = function(){
     var t_duration = this.duration;
     var c_duration = this.currentTime;
 
-    var time = document.getElementById("time");
-    time.innerHTML= parseInt(c_duration * 100/t_duration) +"/"+ t_duration;
 
-    var slide_bar = parseInt(c_duration * 100/t_duration);
+    var sec = c_duration - parseInt(c_duration/60)*60;
+    var t_sec = t_duration - parseInt(t_duration/60)*60;
+    var time = document.getElementById("time");
+    time.innerHTML= parseInt(c_duration/60) +":"+ parseInt(sec) +" / " + parseInt(t_duration/60) +":"+ parseInt(t_sec) ;
+
+    var slide_bar = c_duration * 100/t_duration;
     var progress_bar = document.getElementById("progress_bar");
     progress_bar.style.width = slide_bar +"%";
 
